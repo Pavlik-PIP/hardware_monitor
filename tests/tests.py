042a3289286007793c50308065cc1685878c3a63
Pavlotskiy Ivan <pavlotskiy.iv@gmail.com>
2021-09-13 10:09:41 +0300

    Prepare tests folder

---
+++ tests/tests.py
@@ -0,0 +1,9 @@
+from hardware_monitor.hardware_info import LinuxHardwareInfo
+from hardware_monitor.hardware_logger import HardwareLogger
+
+import psutil
+
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
